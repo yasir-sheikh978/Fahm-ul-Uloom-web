85e4e656acee2db58a542b0eb607fc50468a3938
yasir-sheikh978 <yasirsheikhlive@gmail.com>
2024-11-01 14:28:15 -0700

    new

--- src/compunentIndex/Footer.jsx
+++ src/compunentIndex/Footer.jsx
@@ -7,7 +7,7 @@ import logoTwo from '../assets/logo/logoTwo.png'
 export default function Footer(){
     return(
         <div className=" -z-20 bg-gray-300" >
-            <section className="py-10 bg-blue-950 sm:pt-16 lg:pt-24" data-aos="zoom-in">
+            <section className="py-10 bg-blue-950 sm:pt-16 lg:pt-24" data-aos="zoom-in-down">
     <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
         <div className="grid grid-cols-2 md:col-span-3 lg:grid-cols-6 gap-y-16 gap-x-12">
             <div className="col-span-2 md:col-span-3 lg:col-span-2 lg:pr-8">
