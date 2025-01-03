b57457f5a4e2529852e2b3fe5a7e2960475911e6
yasir-sheikh978 <yasirsheikhlive@gmail.com>
2025-01-03 05:24:46 +0500

    more

--- src/compunentIndex/Whatsapp.jsx
+++ src/compunentIndex/Whatsapp.jsx
@@ -12,13 +12,13 @@ export default function Whatsapp() {
   return (
     <div className="w-[100%] h-20 px-4 flex justify-end fixed bottom-0 z-50" >
       {/* Chat Box */}
-      <div className="absolute bottom-24 right-4 bg-green-100 border border-green-500 rounded-lg p-3 shadow-lg">
+      <div className="absolute bottom-20 right-4 bg-green-100 border border-green-500 rounded-lg p-2 shadow-lg">
         <p className="text-gray-800 font-medium">How can I help you?</p>
       </div>
 
       {/* WhatsApp Icon */}
       <div className="cursor-pointer" onClick={navigateToWhatsApp}>
-        <img src={pic} alt="whatsapp" className="h-20" />
+        <img src={pic} alt="whatsapp" className="h-16 sm:h-20" />
       </div>
     </div>
   );
